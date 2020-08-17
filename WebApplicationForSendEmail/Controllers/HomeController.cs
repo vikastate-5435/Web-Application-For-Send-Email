659d5730dc1786047c119a3442d08f3f00cbd69c
vikastate-5435 <67470492+vikastate-5435@users.noreply.github.com>
2020-08-17 16:18:31 +0530

    Add project files.

---
+++ WebApplicationForSendEmail/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationForSendEmail.Models;
+
+namespace WebApplicationForSendEmail.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(EmailViewModel emailObj)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    string fEmail = ConfigurationManager.AppSettings["fEmail"].ToString();
+                    string fEmailPwd = ConfigurationManager.AppSettings["fEmailPwd"].ToString();
+
+                    MailMessage ms = new MailMessage(fEmail, emailObj.ToEmailId);
+                    ms.Subject = emailObj.Subject;
+                    ms.Body = emailObj.BodyText;
+                    ms.IsBodyHtml = true;
+                    SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                    client.Credentials = new System.Net.NetworkCredential() { UserName = fEmail, Password = fEmailPwd };
+                    client.EnableSsl = true;
+                    client.Send(ms);
+
+                    ViewBag.message = "Email sent successfully";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.message = "Email sent failed";
+                    ModelState.AddModelError("", ex.Message);
+                }
+            }
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplicationForSendEmail/Models/EmailViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationForSendEmail.Models
+{
+    public class EmailViewModel
+    {
+
+        [Required(ErrorMessage ="Please email address for send email")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        public string ToEmailId { get; set; }
+
+        [Required(ErrorMessage ="Please enter subject")]
+        [StringLength(150)] 
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage ="Please enter email text")]
+        [StringLength(5000)] 
+        public string BodyText { get; set; }
+    }
+}
